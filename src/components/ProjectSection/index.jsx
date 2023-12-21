7f05d36cb2655a401f7ffb90805b09fe49d51744
saroz savar <sarojsabar12345@gmail.com>
2023-12-21 14:12:46 +0530

    add college project section

---
+++ src/components/ProjectSection/index.jsx
@@ -0,0 +1,14 @@
+
+
+const ProjectSection = () => {
+  return (
+    <section className='container px-20 py-20 bg-slate-100 gap-8 ' >
+        <h2 className="font-bold text-2xl text-teal-700" >College Projects </h2>
+        <div className="" >
+
+        </div>
+    </section>
+  )
+}
+
+export default ProjectSection
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import SkillSection from '../components/SkillsSection'
+import ProjectSection from '../components/ProjectSection'
 
 const Home = () => {
   return (
     <div className='container bg-slate-400'>
         <HeroSection/>
         <SkillSection/>
+        <ProjectSection/>
     </div>
   )
 }
