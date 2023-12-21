9352d525d3dd91ce7a34820005c8ef3c923aa261
saroz savar <sarojsabar12345@gmail.com>
2023-12-21 14:57:03 +0530

    hello from server

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -12,15 +12,15 @@ const HeroSection = () => {
             <p className='text-base text-teal-700 font' >Enthusiastic web developer with foundational skills in both frontend and backend development, including a solid understanding of the MVC architecture and experience creating REST APIs. 
             Proficient in building frontend interfaces using React functional components and efficiently fetching data from the backend.</p>
             <div className='mt-7 flex items-center gap-6' >
-                <a href='#'  className='flex items-center gap-2 bg-teal-300 px-3 py-2 rounded-sm cursor-pointer ' >
+                <a href='#' target="_blank"  className='flex items-center gap-2 bg-teal-300 px-3 py-2 rounded-sm cursor-pointer ' >
                     <i className="fa-brands fa-instagram text-2xl"></i>
                     <p className='text-lg' >Instagram</p>
                 </a>
-                <a href='https://www.linkedin.com/in/saroj-sabar-dev/' className='flex items-center gap-2 bg-teal-300 px-3 py-2 rounded-sm cursor-pointer ' >
+                <a href='https://www.linkedin.com/in/saroj-sabar-dev/' target="_blank" className='flex items-center gap-2 bg-teal-300 px-3 py-2 rounded-sm cursor-pointer ' >
                     <i class="fa-brands fa-linkedin text-2xl"></i>
                     <p className='text-lg' >Linkedin</p>
                 </a>
-                <a href='https://github.com/sr-81S' className='flex items-center gap-2 bg-teal-300 px-3 py-2 rounded-sm cursor-pointer '>
+                <a href='https://github.com/sr-81S' target="_blank" className='flex items-center gap-2 bg-teal-300 px-3 py-2 rounded-sm cursor-pointer '>
                     <i class="fa-brands fa-github text-2xl"></i>
                     <p className='text-lg' >GitHub</p>
                 </a>
